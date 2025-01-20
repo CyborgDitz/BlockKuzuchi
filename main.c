767602647c42276695d074591bd42f60301967e1
CyborgDitz <sebban.lol@gmail.com>
2025-01-20 15:02:17 +0100

    added a player block

--- main.c
+++ main.c
@@ -4,7 +4,6 @@
 
 #define WINDOW_HEIGHT 1024
 #define WINDOW_WIDTH 1024
-
 #define TILE_WIDTH 32
 #define TILE_HEIGHT 32
 int32_t map[WINDOW_HEIGHT][WINDOW_WIDTH];
@@ -12,6 +11,7 @@ int32_t map[WINDOW_HEIGHT][WINDOW_WIDTH];
 typedef struct {
     bool destroyed;
 } BlockData;
+
 typedef struct {
     Vector2 position;
     float playerSpeed;
@@ -19,11 +19,17 @@ typedef struct {
 
 void DrawTutorial(void);
 void DrawWindow(int32_t *map);
+void DrawPlayer(Player *player);
 
 typedef struct {
     void (*DrawTutorial)(void);
     void (*DrawWindow)(int32_t *map);
+    void (*DrawPlayer)(Player *player); // Corrected function pointer declaration
 } Drawings;
+
+void DrawPlayer(Player *player) {
+    DrawRectangle(player->position.x, player->position.y, TILE_WIDTH * 2, TILE_HEIGHT * 5, PURPLE);
+}
 
 void DrawTutorial() {
     DrawRectangle(10, 10, 250, 113, Fade(SKYBLUE, 0.5f));
@@ -35,17 +41,18 @@ void DrawTutorial() {
 void DrawWindow(int32_t *map) {
     for (int i = 0; i < WINDOW_HEIGHT; i++) {
         for (int j = 0; j < WINDOW_WIDTH; j++) {
-
             if (*(map + i * WINDOW_WIDTH + j) == 1) {
                 DrawRectangle(j * TILE_WIDTH, i * TILE_HEIGHT, TILE_WIDTH, TILE_HEIGHT, WHITE);
             }
         }
     }
 }
+
 void UpdatePlayer(Player *player, float delta) {
-    if (IsKeyDown(KEY_LEFT)) player->position.x -= delta;
-    if (IsKeyDown(KEY_RIGHT)) player->position.x += delta;
-};
+    if (IsKeyDown(KEY_LEFT)) player->position.x -= player->playerSpeed * delta;
+    if (IsKeyDown(KEY_RIGHT)) player->position.x += player->playerSpeed * delta;
+}
+
 int main(void)
 {
     InitWindow(800, 600, "BlockKuzuchi");
@@ -61,13 +68,24 @@ int main(void)
     Drawings myDrawings;
     myDrawings.DrawTutorial = DrawTutorial;
     myDrawings.DrawWindow = DrawWindow;
+    myDrawings.DrawPlayer = DrawPlayer;
+
+    Player player = {0};
+    player.position = (Vector2) {100, 100};
+    player.playerSpeed = 4.0f;
+
+    SetTargetFPS(60);
 
     while (!WindowShouldClose()) {
+        float deltaTime = GetFrameTime();
+        UpdatePlayer(&player, deltaTime);
+
         BeginDrawing();
         ClearBackground(YELLOW);
 
         myDrawings.DrawWindow((int32_t *)map);
         myDrawings.DrawTutorial();
+        myDrawings.DrawPlayer(&player);
 
         EndDrawing();
     }
