e4b831ac31129b4bf8b2efb5e7a0257a815ea6ff
CyborgDitz <sebban.lol@gmail.com>
2025-01-20 15:32:58 +0100

    Player moves with velocity

--- main.c
+++ main.c
@@ -14,8 +14,10 @@ typedef struct {
 
 typedef struct {
     Vector2 position;
+    Vector2 velocity;
     float playerSpeed;
 } Player;
+
 
 void DrawTutorial(void);
 void DrawWindow(int32_t *map);
@@ -54,25 +56,31 @@ void DrawWindow(int32_t *map) {
         }
     }
 }
-void UpdatePlayer(Player *player, float deltaTime)
-
-    {
-        if (IsKeyDown(KEY_A))
-        {
-            player->position.x -= player->playerSpeed * deltaTime;
-        }
-        if (IsKeyDown(KEY_D))
-        {
-            player->position.x += player->playerSpeed * deltaTime;
-        }
+void UpdatePlayer(Player *player, float deltaTime) {
+    if (IsKeyDown(KEY_A)) {
+        player->velocity.x = -player->playerSpeed;
+    } else if (IsKeyDown(KEY_D)) {
+        player->velocity.x = player->playerSpeed;
+    } else {
+        player->velocity.x = 0;
     }
+
+    if (IsKeyDown(KEY_W)) {
+        player->velocity.y = -player->playerSpeed;
+    } else if (IsKeyDown(KEY_S)) {
+        player->velocity.y = player->playerSpeed;
+    } else {
+        player->velocity.y = 0;  // No vertical movement
+    }
+
+    player->position.x += player->velocity.x * deltaTime;
+    player->position.y += player->velocity.y * deltaTime;
+}
 
 int main(void)
 {
     InitWindow(800, 600, "BlockKuzuchi");
-
-    Player player = InitializePlayer((Vector2){100, 100}, 69.0f);
-
+    Player player = InitializePlayer((Vector2){100, 100}, 300.0f);
     for (int i = 0; i < WINDOW_HEIGHT; i++) {
         for (int j = 0; j < WINDOW_WIDTH; j++) {
             if ((i * WINDOW_WIDTH + j) % 2 == 0) {
@@ -85,6 +93,7 @@ int main(void)
     myDrawings.DrawTutorial = DrawTutorial;
     myDrawings.DrawWindow = DrawWindow;
     myDrawings.DrawPlayer = DrawPlayer;
+    Player player = InitializePlayer((Vector2){100, 100}, 300.0f);
 
     while (!WindowShouldClose()) {
         float deltaTime = GetFrameTime();
@@ -99,7 +108,6 @@ int main(void)
 
         EndDrawing();
     }
-
     CloseWindow();
     return 0;
 }
