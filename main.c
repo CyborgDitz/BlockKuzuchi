ed4be4a7d2e4488b589d02f78c9d1e8fd5395efd
CyborgDitz <sebban.lol@gmail.com>
2025-01-20 14:42:45 +0100

    Reordered the code arrangement. Started implementing player move

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include <stdint.h>
 #include <stdio.h>
-
 #include "raylib.h"
 
 #define gWindowHeight 1024
@@ -8,16 +7,15 @@
 
 #define gTileWidth 32
 #define gTileHeight 32
-
-typedef struct {
-    int32_t x, y;
-} position;
-
 int32_t map[gWindowHeight][gWindowWidth];
 
 typedef struct {
     bool destroyed;
-} blockData;
+} BlockData;
+typedef struct {
+    Vector2 position;
+    float playerSpeed;
+}Player;
 
 void DrawTutorial(void);
 void DrawWindow(int32_t *map);
@@ -25,37 +23,7 @@ void DrawWindow(int32_t *map);
 typedef struct {
     void (*DrawTutorial)(void);
     void (*DrawWindow)(int32_t *map);
-} drawings;
-
-int main(void)
-{
-    InitWindow(800, 600, "BlockKuzuchi");
-
-    for (int i = 0; i < gWindowHeight; i++) {
-        for (int j = 0; j < gWindowWidth; j++) {
-            if ((i * gWindowWidth + j) % 2 == 0) {
-                map[i][j] = 1;
-            }
-        }
-    }
-
-    drawings myDrawings;
-    myDrawings.DrawTutorial = DrawTutorial;
-    myDrawings.DrawWindow = DrawWindow;
-
-    while (!WindowShouldClose()) {
-        BeginDrawing();
-        ClearBackground(YELLOW);
-
-        myDrawings.DrawWindow((int32_t *)map);
-        myDrawings.DrawTutorial();
-
-        EndDrawing();
-    }
-
-    CloseWindow();
-    return 0;
-}
+} Drawings;
 
 void DrawTutorial() {
     DrawRectangle(10, 10, 250, 113, Fade(SKYBLUE, 0.5f));
@@ -73,4 +41,37 @@ void DrawWindow(int32_t *map) {
             }
         }
     }
+}
+void UpdatePlayer(Player *player, EnvItem *envItems, int envItemsLength, float delta) {
+    if (IsKeyDown(KEY_LEFT)) player->position.x -= PLAYER_HOR_SPD*delta;
+    if (IsKeyDown(KEY_RIGHT)) player->position.x += PLAYER_HOR_SPD*delta;
+};
+int main(void)
+{
+    InitWindow(800, 600, "BlockKuzuchi");
+
+    for (int i = 0; i < gWindowHeight; i++) {
+        for (int j = 0; j < gWindowWidth; j++) {
+            if ((i * gWindowWidth + j) % 2 == 0) {
+                map[i][j] = 1;
+            }
+        }
+    }
+
+    Drawings myDrawings;
+    myDrawings.DrawTutorial = DrawTutorial;
+    myDrawings.DrawWindow = DrawWindow;
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(YELLOW);
+
+        myDrawings.DrawWindow((int32_t *)map);
+        myDrawings.DrawTutorial();
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
 }
